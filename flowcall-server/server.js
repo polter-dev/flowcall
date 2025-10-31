8ef9d01771e5dc12545c559b101d276459e17160
Melvin Roger <mcr3649@gmail.com>
2025-10-30 22:29:53 -0400

    added flowcall-server

---
+++ flowcall-server/server.js
@@ -0,0 +1,55 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+    },
+});
+
+const rooms = {};
+io.on("connection", (socket) => {
+    console.log("User connected:", socket.id);
+
+    
+    socket.on ("create-room", () => {
+        const code = Math.random().toString(36).substring(2,8)
+        rooms[code] = [socket.id];
+        socket.join(code);
+        socket.emit("room-created", code);
+    });
+
+    socket.on("join-room", (code) => {
+        if(rooms[code]){
+            rooms[code].push(socket.id);
+            socket.join(code);
+            io.to(code).emit("user-joined", socket.id);
+        } else {
+            socket.emit("error", "Room does not exist");
+        }
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected:", socket.id);
+        for (const code in rooms){
+            rooms[code] = rooms[code].filter((id) => id !== socket.id);
+            if(rooms[code].length === 0) delete rooms[code];
+        }
+    });
+});
+
+app.get("/", (req, res) => {
+    res.send("Flowcall Room Server is running");
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
