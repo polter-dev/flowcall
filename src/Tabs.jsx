14390e0d0343faaeae77706f6fc5614f43225344
polter-dev <144177408+polter-dev@users.noreply.github.com>
2025-11-05 11:42:59 -0500

    added tabs index

--- src/Tabs.jsx
+++ src/Tabs.jsx
@@ -1,6 +1,8 @@
 //import necessary libraries for creating tabs
 
 const TabGroup = () => {
+    
+    //tab labels
     const tabs = ["Host" , "Join"];
 
 
@@ -9,11 +11,14 @@ const TabGroup = () => {
     return (
         <div>
             <div className='flex gap-3 justify-center items-center'>
-                {tabs.map((tabs => <button>{tabs}</button>))}
+                {tabs.map((tabs => <button className = 'px-4 border py-3 hover:bg-blue300'>
+                    {tabs}</button>))}
             </div>
 
-            <div className =''>
-
+            <div className ='flex flex-col mx-auto'>
+                    <div>Content One</div>
+                    <div>Content Two</div>
+                    <div>Content Three</div>
             </div>
         </div>
     )
